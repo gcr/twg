04b308c94f48e1864d5609d9b7319f84cd433386
Michael W <hotdog003@gmail.com>
2009-07-01 21:44:36 -0600

    Added the admin interface.

--- settings.py
+++ settings.py
@@ -80,13 +80,14 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/michael/Project/projects/twg/templates",
 )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
+    'django.contrib.admin',
     'twg.stories',
-    'django.contrib.admindocs',
 )
--- stories/models.py
+++ stories/models.py
@@ -1,8 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Story(models.Model):
     name = models.CharField(max_length = 50)
+    last_update_date = models.DateTimeField()
+    
+    
+    def __unicode__(self):
+        return self.fulltext
+        
+        
+    @property
+    def fulltext(self):
+        frag_list = self.fragment_set.all()
+        if frag_list:
+            return " ".join([fragment.text for fragment in frag_list])
+        else:
+            return ""
+        
+        
+    def add_fragment(self, new_text, new_author, date=None):
+        if not date:
+            date = datetime.datetime.now()
+            
+        word_list = [
+            word.strip()
+            for word
+            in new_text.strip().split(" ")
+            if word.strip() != ""
+        ]
+        
+        if len(word_list) > 3:
+            raise ValueError("Fragments can only be three words long!")
+            
+        self.fragment_set.add(Fragment(
+            text=" ".join(word_list),
+            author=new_author,
+            date=datetime.datetime.now(),
+            story=self
+        ))
+        
+        self.last_update_date = date
+        self.save()
+        
+        
+    class Meta:
+        ordering = ("last_update_date", "name")
+        verbose_name_plural = "Stories"
+
     
 class Fragment(models.Model):
     text = models.CharField(max_length = 50)
