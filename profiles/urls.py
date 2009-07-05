a487ba460660b3bfed8e74f9a62fe99e88c10446
Michael W <hotdog003@gmail.com>
2009-07-05 11:08:50 -0600

    Framework in place for editing profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,8 +4,13 @@ import views
 # Generic views ---------------
 urlpatterns = patterns('',
     url(
-        r'^(?P<username>[\w-]+)/?', views.view_profile,
+        r'^(?P<username>[\w\d-]+)/?$', views.view_profile,
         name='profile_view'
+    ),
+    
+    url(
+        r'^edit/(?P<username>[\w\d-]+)/?$', views.edit_profile,
+        name='profile_edit'
     ),
 )
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from stories.models import Story
 from django.template import RequestContext
 
@@ -14,3 +15,12 @@ def view_profile(request, username):
     context = RequestContext(request, {'profile_user': user, 'stories':stories})
     return render_to_response('profiles/view_user.html', context)
     #return HttpResponse("Hello, " + username)
+    
+@login_required
+def edit_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    if user != request.user:
+        # Fail because they can't edit other profiles
+        return HttpResponseRedirect("/")
+    return HttpResponse("Editing your profile...")
+    
